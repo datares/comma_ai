e6331420abfd234f4859768c26b6ef93f4786983
mrcolo <colo28197@gmail.com>
2020-02-04 00:29:56 -0800

    merge

--- datasets.py
+++ datasets.py
@@ -13,7 +13,6 @@ if not os.path.exists('./train/'):
 if not os.path.exists('./train_path/'):
     os.makedirs('./train_path')
 
-
 # TODO we should use a function instead of having a global variable data
 # Load the labels into python with using a pandas DataFrame
 data = pd.read_csv('traintxt.txt', sep=" ", header=None)
@@ -24,19 +23,32 @@ data.columns = ['label','frame']
 # swap the columns so that the dataframe reads, more conventionally, item name on the left and label on the right
 data = data[['frame','label']]
 
-def video_to_frames(video: str) -> str:
+# loads the label data into a pandas.DataFrame object
+def load_data_labels(filename: str) -> pd.DataFrame:
+	# Load the labels into python with using a pandas DataFrame
+	data = pd.read_csv(filename, sep=" ", header=None)
+	# keep track of what frame corresponds to what speed
+	data['frame'] = data.index
+	# name the speed column "label"
+	data.columns = ['label','frame']
+	# swap the columns so that the dataframe reads, more conventionally, item name on the left and label on the right
+	data = data[['frame','label']]
+	# return the dataFrame object
+	return data
+
+def video_to_frames(video: str, destination='.') -> str:
 	vidcap = cv2.VideoCapture(video)
 	success,image = vidcap.read()
 	count = 0
 
 	# write raw frames to local files
 	while success:
-		cv2.imwrite('./train/frame%d.jpg' % count, image)     # save frame as JPEG file      
+		cv2.imwrite(destination + '/train/frame%d.jpg' % count, image)     # save frame as JPEG file
 		success,image = vidcap.read()
 		print('Read a new frame: ', success)
 		count += 1
 
-def enhance_frames(train_path: str):
+def enhance_frames(train_path: str, destination_path='.'):
     #enhance the files, copy them into a different folder
     for img in os.listdir(train_path):
         im = Image.open('{}/{}'.format(train_path, img))
@@ -50,8 +62,7 @@ def enhance_frames(train_path: str):
         sharpness = ImageEnhance.Sharpness(im)
         im = sharpness.enhance(2)
 
-        im = im.save('trainbright/%s' %img)
-
+        im = im.save(destination_path + '/trainbright/%s' %img)
 
 # define a custom PyTorch dataset that can be used to feed into a DataLoader
 class imageDataset(Dataset):
@@ -75,28 +86,30 @@ class imageDataset(Dataset):
         return im, label
 
 if __name__ == "__main__":
+    # command argument parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-d', '--data_path', type=str, default='.',
+		help='Path to the data folder.')
+	args = parser.parse_args()
+	data_folder_path = args.data_path
+
     # transform the image into something that can be fed to a neural network - we convert to tensor because neural networks want arrays of pixels
     train_transform = transforms.Compose([
         transforms.ToTensor()
         #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) <-- this is imagenet
         ])
 
+    # load the data labels into a pandas DataFrame
+	data_labels = load_data_labels(data_folder_path + '/traintxt.txt')
     #initialize a training dataset
     trainset = imageDataset(data, 'trainbright', transform =train_transform)
 
-    # to test --> print(trainset.__getitem__(0))
+	# we don't shuffle yet because the order of the data matters
+	# TODO we should probably shuffle, but pick batches of images that are related to themselves.
+	# TODO how do we do this with pytorch?
+	train_loader = DataLoader(trainset, batch_size=1, shuffle=False, num_workers=1)
 
-    # create a dataloader - batch_size and num_workers will have to be tuned as time goes on
-
-    # we don't shuffle yet because the order of the data matters
-    # TODO we should probably shuffle, but pick batches of images that are related to themselves. 
-    # TODO how do we do this with pytorch?
-
-    # TODO change batch_size
-    # TODO change num_workers, your cpu has more cores for a reason!
-    train_loader = DataLoader(trainset, batch_size=1, shuffle=False, num_workers=1)
-
-    # TODO can we do this with PyTorch/sklearn(!!!!)?
-    # one example of how to create a validation set - sloppy, but this is one way to do it
-    val = data[16000:]
-    val.reset_index(drop=True,inplace=True)
+	# TODO can we do this with PyTorch/sklearn(!!!!)?
+	# one example of how to create a validation set - sloppy, but this is one way to do it
+	val = data_labels[16000:]
+	val.reset_index(drop=True,inplace=True)
